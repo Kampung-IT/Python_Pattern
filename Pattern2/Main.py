9efe6826120e36b01679d5853b2dcc54fb78a7f9
Kampung IT <74341961+Kampung-IT@users.noreply.github.com>
2020-11-13 13:01:20 +0700

    Create Main.py

---
+++ Pattern2/Main.py
@@ -0,0 +1,7 @@
+baris = 10
+for row in range(1, baris+1):
+    for column in range(1, row + 1):
+        print(column, end=' ') # tampilkan nomor
+    # baris demi baris untuk menampilkan pola dengan benar
+    print("")				 
+	 
