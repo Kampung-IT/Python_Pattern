8689da94754c87dde93f8ae62d6e77efc677d6ac
Kampung IT <74341961+Kampung-IT@users.noreply.github.com>
2020-11-13 12:57:39 +0700

    Create Main.py

---
+++ Pattern1/Main.py
@@ -0,0 +1,7 @@
+jumlah = 10
+for nomor in range(jumlah):
+    for i in range(nomor):
+        print(nomor, end=" ")  # tampilkan nomor
+    # baris demi baris untuk menampilkan pola dengan benar
+    print(" ")	
+	
